db8c6b19c18a0668b1a7387928009c76f6046390
Azzam <azzam@example.com>
2026-04-01 15:24:16 +0200

    fix(server): decouple Matrix to isolate 502

--- packages/twenty-server/src/modules/modules.module.ts
+++ packages/twenty-server/src/modules/modules.module.ts
@@ -4,7 +4,7 @@ import { CalendarModule } from 'src/modules/calendar/calendar.module';
 import { ConnectedAccountModule } from 'src/modules/connected-account/connected-account.module';
 import { FavoriteFolderModule } from 'src/modules/favorite-folder/favorite-folder.module';
 import { FavoriteModule } from 'src/modules/favorite/favorite.module';
-import { MatrixModule } from 'src/modules/matrix/matrix.module';
+// import { MatrixModule } from 'src/modules/matrix/matrix.module';
 import { MessagingModule } from 'src/modules/messaging/messaging.module';
 import { WorkflowModule } from 'src/modules/workflow/workflow.module';
 import { WorkspaceMemberModule } from 'src/modules/workspace-member/workspace-member.module';
@@ -18,7 +18,7 @@ import { WorkspaceMemberModule } from 'src/modules/workspace-member/workspace-me
     FavoriteFolderModule,
     FavoriteModule,
     WorkspaceMemberModule,
-    MatrixModule,
+    // MatrixModule,
   ],
   providers: [],
   exports: [],
